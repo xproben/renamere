a09875470916d0bd30cadae35e08c6906cf105b1
GRoot <124968285+xproben@users.noreply.github.com>
2023-03-04 00:21:58 -0800

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -4,19 +4,19 @@ from pyrogram import Client , filters
 
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
-	text = """**Free Plan User**"""
+	text = """**Free Plan User To upgrade to VIP \n Ask for Vip Plan On <a href='t.me/HeavenBotSupport'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>**"""
 	keybord = InlineKeyboardMarkup([[ 
         			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/xdecoy")], 
-        			[InlineKeyboardButton("PayPal 🌎",url = "https://www.paypal.me/"),
-        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			[InlineKeyboardButton("Bot Support 🌎",url = "https://t.me/HeavenBotSupport"),
+        			InlineKeyboardButton("Bot Update Channel ",url = "https://t.me/RestinHeavenBots")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
-	text = """**Free Plan User**"""
+	text = """<b>Free Plan User \n To upgrade to VIP \n Ask for Vip Plan On <a href='t.me/HeavenBotSupport'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>"""
 	keybord = InlineKeyboardMarkup([[ 
         			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/xdecoy")], 
-        			[InlineKeyboardButton("PayPal 🌎",url = "https://www.paypal.me/"),
-        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			[InlineKeyboardButton("Bot Support 🌎",url = "https://t.me/HeavenBotSupport"),
+        			InlineKeyboardButton("Bot Update Channel ",url = "https://t.me/RestinHeavenBots")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await message.reply_text(text = text,reply_markup = keybord)
